6a28dc89933759e3e4210cbd222bbe3fd37e1263
Ashbee Morgado <ashbee.morgado@icloud.com>
2020-06-18 09:12:21 +0800

    Update send-payslip.blade.php

--- resources/views/email/send-payslip.blade.php
+++ resources/views/email/send-payslip.blade.php
@@ -8,7 +8,7 @@ The password for the attachment is: <b>{{$password}}</b>
 Thanks,<br>
 
 Finance and Accounting Department <br>
-
+{{route('view.logo')}}?id={{$payroll->id}}
 <img src="{{route('view.logo')}}?id={{$payroll->id}}" style="max-width:20%">
 {{-- <img src="https://payslip.light.org.ph/get/logo?id={{$payroll->id}}" style="max-width:20%"> --}}
 @endcomponent
