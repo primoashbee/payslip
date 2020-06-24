fe70a7f72835ffca0350f74fe7193835740e7d13
Ashbee Morgado <ashbee.morgado@icloud.com>
2020-06-24 14:09:16 +0800

    Update send-payslip.blade.php

--- resources/views/email/send-payslip.blade.php
+++ resources/views/email/send-payslip.blade.php
@@ -1,9 +1,3 @@
-@slot('footer')
-    @component('mail::footer')
-    <img src="{{$logo}}" class="logo" alt="LIGHT Logo">
-    @endcomponent
-@endslot
-
 @component('mail::message')
 # Hello, {{$payroll->name}}!
 
@@ -16,8 +10,3 @@ Thanks,<br>
 Finance and Accounting Department <br>
 <img src="https://payslip.light.org.ph/get/logo?id={{$payroll->id}}" style="max-width:20%">
 @endcomponent
-@slot('footer')
-    @component('mail::footer')
-        © {{ date('Y') }} LIGHT Microfinance Inc. All rights reserved.
-    @endcomponent
-@endslot
